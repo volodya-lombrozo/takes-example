8ab540d81e8db30a71faced75dc7385c98561745
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-05-04 12:58:08 +0300

    Add json response

--- src/main/java/org/example/takes_example/App.java
+++ src/main/java/org/example/takes_example/App.java
@@ -1,8 +1,50 @@
 package org.example.takes_example;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.facets.fork.FkRegex;
+import org.takes.facets.fork.TkFork;
+import org.takes.http.Exit;
+import org.takes.http.FtBasic;
+import org.takes.rq.RqHref;
+import org.takes.rs.RsText;
+import org.takes.rs.RsWithType;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final AtomicLong COUNTER = new AtomicLong(0L);
+
+    public static void main(String[] args) throws IOException {
+        new FtBasic(
+            new TkFork(
+                new FkRegex("/", "hello, world!"),
+                new FkRegex(
+                    "/message",
+                    new JsonResponse()
+                )
+            ),
+            8080
+        ).start(Exit.NEVER);
+    }
+
+    private static class JsonResponse implements Take {
+
+        @Override
+        public Response act(final Request req) throws IOException {
+            return new RsWithType(
+                new RsText(
+                    String.format(
+                        "{\"id\":\"%d\",\"client_timestamp\":\"%s\",\"timestamp\":\"%d\"}",
+                        App.COUNTER.incrementAndGet(),
+                        new RqHref.Smart(req).single("timestamp"),
+                        System.currentTimeMillis()
+                    )
+                ),
+                "application/json"
+            );
+        }
     }
 }
